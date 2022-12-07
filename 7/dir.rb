a2dc9739d70c1b2e7874eb7b106fd687a3cbb46c
Zachary Danger Campbell <zacharydanger@fastmail.com>
2022-12-07 08:47:19 -0600

    honestly, fuck day 7

---
+++ 7/dir.rb
@@ -0,0 +1,99 @@
+def unfuck_it(file)
+  files = {}
+
+  dir_sizes = {}
+
+  current_dir = nil
+
+  File.readlines(file).each do |line|
+    line.strip!
+    if line.start_with? '$'
+      _sh, command, arg = line.split(' ')
+
+      puts "COMMAND: #{command} #{arg}"
+
+      if command == 'cd'
+        if arg == '/'
+          current_dir = ['root']
+        elsif arg == '..'
+          current_dir.pop
+        else
+          current_dir << arg
+        end
+
+        puts "current_dir: #{current_dir.join('/') || '/'}"
+      end
+    elsif line.start_with? /\d/
+      puts "FILE: #{line}"
+      size, name = line.split(' ')
+
+      file_name = (current_dir + [name]).join('/')
+
+      files[file_name] = size.to_i
+    else
+      puts "OUTPUT:  #{line}"
+    end
+  end
+
+  files
+end
+
+def dir_totals(files)
+  puts
+  puts 'calc'
+  puts
+
+  totals = {}
+
+  files.each do |fpath, size|
+    fpath = fpath.split('/')
+    _fname = fpath.pop
+
+    fpath.each do |dir|
+      fpath.index(dir).times do |i|
+        slice = fpath.slice(0..i+1).join('/')
+        totals[slice] ||= 0
+        totals[slice] += size
+      end
+    end
+  end
+
+  totals
+end
+
+fs = 70000000
+rs = 30000000
+
+files = unfuck_it('./input')
+
+pp files
+
+used_space = files.values.sum
+
+free_space = fs - used_space
+
+need_to_free = rs - free_space
+
+puts "Used Space: #{used_space}"
+puts "Free Space: #{free_space}"
+puts "Needed:     #{rs - free_space}"
+
+dirs = files.keys.map do |fpath|
+  fpath.split('/').tap do |f|
+    f.pop
+  end.join('/')
+end
+
+dir_totes = {}
+
+dirs.each do |dir|
+  dir_totes[dir] = files.select { |k,v|
+    k.start_with? dir
+  }.values.sum
+end
+
+dir_totes.select! { |k,v|
+ v >= need_to_free
+}
+
+pp dir_totes.values.sort.first
